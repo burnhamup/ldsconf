55736f9bb492b9cd81b0f5cff345b3000c9cf23e
Chris Burnham <chris@burnhamup.com>
2015-10-25 14:49:14 -0700

    Removed reports and sustainings.

--- ldsconf/main.py
+++ ldsconf/main.py
@@ -5,10 +5,18 @@ from ldsconf.studyplan import generate_study_plan
 __author__ = 'Chris'
 
 #
-# conferences = Conference.get_all_conferences()
-# Conference.save(conferences)
-results = generate_study_plan(10, 2015)
-for plan in results:
-    print "Plan"
-    for date, talk in plan:
-        print "%s - %s, %s (%s, %s)" % (date, talk.title, talk.author, talk.year, talk.month)
+conferences = Conference.get_all_conferences()
+for conference in conferences.itervalues():
+    def keep_talk(talk):
+        if 'Presented' in talk.author or 'Statistical Report' in talk.title or 'Church Audit' in talk.title or 'Sustaining of Church Officers' in talk.title or 'Church Finance Committee Report' in talk.title:
+            return False
+        return True
+    conference.talks = filter(keep_talk, conference.talks)
+Conference.save(conferences)
+
+
+# results = generate_study_plan(10, 2015)
+# for plan in results:
+#     print "Plan"
+#     for date, talk in plan:
+#         print "%s - %s, %s (%s, %s)" % (date, talk.title, talk.author, talk.year, talk.month)

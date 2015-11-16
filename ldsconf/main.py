790778ed31fcde94e78d02f3e94a90c93d050a18
Chris Burnham <chris@burnhamup.com>
2015-11-15 20:10:36 -0800

    Working setup.py
    Install appears to be working

--- ldsconf/main.py
+++ ldsconf/main.py
@@ -1,17 +1,8 @@
-"""
-    Questions to ask:
-    Start Date
-    End Date
-    Calculate number of days
-    Calculate number of talks in the most recent conference.
-    Max number of study periods is equal to days / number of talks.
-    Ask how many study periods to have.
-"""
 import argparse
 import math
-from datetime import datetime
-from pprint import pprint
-import studyplan
+from datetime import datetime, timedelta
+
+from ldsconf import studyplan
 
 
 def valid_date(s):
@@ -22,28 +13,54 @@ def valid_date(s):
         raise argparse.ArgumentTypeError(msg)
 
 
+def get_default_dates():
+    now = datetime.now()
+    if 4 <= now.month < 10:
+        month = 4
+    else:
+        month = 10
+    year = now.year
+    if now.month < 4:
+        year -= 1
+    start_date = studyplan.get_conference_start_date(month, year)
+    start_date += timedelta(days=7)
+
+    next_month = 4 if month == 10 else 10
+    next_year = year + (1 if month == 10 else 0)
+    end_date = studyplan.get_conference_start_date(next_month, next_year)
+    end_date += timedelta(days=7)
+
+    return start_date, end_date
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('start_date', help="Start date for the study plan - format YYYY-MM-DD", default=None, type=valid_date)
-    parser.add_argument('end_date', help="End date for the study plan - format YYYY-MM-DD", default=None, type=valid_date)
-    parser.add_argument('study_periods', help="Number of study periods", default=None, type=int)
+    default_start_date, default_end_date = get_default_dates()
+    parser.add_argument('-s', '--start_date', help="Start date for the study plan - format YYYY-MM-DD",
+                        default=default_start_date, type=valid_date)
+    parser.add_argument('-e', '--end_date', help="End date for the study plan - format YYYY-MM-DD",
+                        default=default_end_date, type=valid_date)
+    parser.add_argument('-n', '--study_periods', help="Number of study periods", default=None, type=int)
     args = parser.parse_args()
 
     # Validation
     if args.start_date >= args.end_date:
         raise Exception("Start date must be before end date")
-    number_of_days =  (args.end_date -args.start_date).days
+    number_of_days = (args.end_date - args.start_date).days
     number_of_talks = studyplan.get_number_of_talks_in_most_recent_conference()
-    maximum_study_plans = math.ceil(number_of_days / float(number_of_talks))
-    if args.study_periods > maximum_study_plans or args.study_periods <= 0:
-        raise Exception("Number of study periods must be in range 1-%d" % maximum_study_plans)
+    maximum_study_periods = math.ceil(number_of_days / float(number_of_talks))
+    study_periods = args.study_periods
+    while study_periods is None:
+        try:
+            study_periods = int(raw_input("How many study periods would like? (1-%d)" % maximum_study_periods))
+        except ValueError:
+            print "Please enter a number."
 
-    result = studyplan.generate_study_plan(args.start_date, args.end_date, args.study_periods)
+    if study_periods > maximum_study_periods or study_periods <= 0:
+        raise Exception("Number of study periods must be in range 1-%d" % maximum_study_periods)
+
+    result = studyplan.generate_study_plan(args.start_date, args.end_date, study_periods)
     for plan in result:
         print "Conference Plan"
         for date, talk in plan:
             print "  %s -- %s, %s (%s-%s)" % (date.strftime("%m-%d-%Y"), talk.title, talk.author, talk.year, talk.month)
-
-
-if __name__ == "__main__":
-    main()
--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
-__author__ = 'Chris'
+from setuptools import setup
+
+setup(
+    name='ldsconf',
+    version='1.0.0',
+    description='LDS General Conference Study Plan Generator',
+    url='https://github.com/burnhamup/ldsconf',
+    author='Chris Burnham',
+    author_email="chris@burnhamup.com",
+    license='MIT',
+    packages=['ldsconf'],
+    install_requires=['lxml', 'requests'],
+    data_files=[('data', ['data/conferences.json'])],
+    entry_points={
+        'console_scripts': [
+            'ldsconf=ldsconf.main:main'
+        ]
+    }
+)
